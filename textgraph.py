70ebe48ce0d987479674fb1fd109abfb61af85a1
chiller13 <91254417+chiller13@users.noreply.github.com>
2021-11-18 19:03:09 +0100

    initial upload

---
+++ textgraph.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import networkx as nx
+import string
+import re
+import matplotlib.pyplot as plt
+from textmetrics import calculatePrevalence
+
+
+def clean_texts(texts, stopwords, **kwargs):
+    """
+    Applies cleaning steps on texts.
+    texts is a list of strings, one for each e-mail analyzed.
+    """
+    # Define stopwords
+    stopw = list(stopwords) + ["‘"]
+
+    print("Convert to lowercase")
+    texts = [t.lower() for t in texts]
+
+    print("Remove words that start with HTTP")
+    texts = [re.sub(r"http\S+", " ", t) for t in texts]
+
+    print("Remove words that start with WWW")
+    texts = [re.sub(r"www\S+", " ", t) for t in texts]
+
+    print("Remove punctuation")
+    regex = re.compile("[%s]" % re.escape(string.punctuation))
+    texts = [regex.sub(" ", t) for t in texts]
+
+    print("Remove words made of single letters")
+    texts = [re.sub(r"\b\w{1}\b", " ", t) for t in texts]
+
+    print("Remove stopwords")
+    pattern = re.compile(r"\b(" + r"|".join(stopw) + r")\b\s*")
+    texts = [pattern.sub(" ", t) for t in texts]
+
+    print("Remove additional whitespaces")
+    texts = [re.sub(" +", " ", t) for t in texts]
+
+    print("Remove Linebreaks")
+    texts = [t.replace("\n", "") for t in texts]
+
+    print("Tokenize text documents (becomes a list of lists)")
+    texts = [t.split() for t in texts]
+
+    stemmer = kwargs.get("stemmer", None)
+    if stemmer:
+        print("Stemming")
+        texts = [[stemmer.stem(w) for w in t] for t in texts]
+
+    return texts
+
+
+def create_wcn(texts, co_range=0, link_filter=2, remove_isolates=False):
+    """Creates Word Co-Occurrence Graph from texts."""
+    # Create an undirected Network Graph
+    G = nx.Graph()
+
+    # Each word is a network node
+    prevalence = calculatePrevalence(texts)
+
+    for word in prevalence.keys():
+        G.add_node(word, prevalence=prevalence[word])
+
+    # Add links based on co-occurrences
+    for doc in texts:
+        w_list = []
+        length = len(doc)
+
+        if co_range == 0:
+            co_range = length  # Betrachtung der gesamten Mail
+
+        for k, w in enumerate(doc):
+            # Define range, based on document length
+            if (k + co_range) >= length:
+                superior = length
+            else:
+                superior = k + co_range + 1
+            # Create the list of co-occurring words
+            if k < length - 1:
+                for i in range(k + 1, superior):
+                    linked_word = doc[i].split()
+                    w_list = w_list + linked_word
+            # If the list is not empty, create the network links
+            if w_list:
+                for p in w_list:
+                    if G.has_edge(w, p):
+                        G[w][p]["weight"] += 1
+                    else:
+                        G.add_edge(w, p, weight=1)
+            w_list = []
+
+    # Create a new Graph which has only links above
+    # the minimum co-occurrence threshold
+    G_filtered = nx.Graph()
+    G_filtered.add_nodes_from(G.nodes(data=True))
+    for u, v, data in G.edges(data=True):
+        if data["weight"] >= link_filter:
+            G_filtered.add_edge(u, v, weight=data["weight"])
+
+    if remove_isolates:
+        print("No. of Nodes:", G_filtered.number_of_nodes(), "No. of Edges:", G_filtered.number_of_edges())
+        return G_filtered
+
+    # Optional removal of isolates
+    isolates = set(nx.isolates(G_filtered))
+
+    G_filtered.remove_nodes_from(isolates)
+
+    # Check the resulting graph (for small test graphs)
+    print("Original Network\nNo. of Nodes:", G.number_of_nodes(), "No. of Edges:", G.number_of_edges())
+    print("Filtered Network\nNo. of Nodes:", G_filtered.number_of_nodes(), "No. of Edges:", G_filtered.number_of_edges())
+    return G_filtered
+
+
+def show_ego_of_word(G, node, radius=1, min_weight=1, figsize=(20, 15)):
+    """Visualizes the ego graph of **node** in **G**."""
+    ego = nx.ego_graph(G, node, radius=radius, center=True, undirected=False)
+    ego.remove_edges_from(nx.selfloop_edges(ego))
+
+    # Falls man nochmal Filtern will
+    if min_weight > 1:
+        ego.remove_edges_from([(u, v, d) for u, v, d in ego.edges(data=True) if d["weight"] < min_weight])
+        component = nx.node_connected_component(ego, node)
+        to_remove = component.symmetric_difference(set(G.nodes))
+        ego.remove_nodes_from(to_remove)
+
+    print("No. of Nodes:", ego.number_of_nodes(), "No. of Edges:", ego.number_of_edges())
+    colors = color_nodes(G=ego, ego_node=node)
+    plt.figure(figsize=figsize)
+    pos = nx.spring_layout(ego, seed=42, weight="weight")
+    labels = nx.draw_networkx_labels(ego, pos=pos, font_color="black")
+    edges = nx.draw_networkx_edges(ego, pos=pos, alpha=0.5)
+
+    nx.draw(ego, pos=pos, node_color=colors, edge_color="black", node_size=[50 * oc for d in ego.nodes() for oc in dict(ego.nodes(data=True))[d].values()])
+    plt.show()
+
+
+def color_nodes(G, ego_node):
+    """Returns a list of colors to plot **G**. Marks **person** and its neighbors in **G**."""
+    colors = ["lightblue" for i in range(len(G.nodes))]
+
+    for ind, node in enumerate(G.nodes):
+        if node in nx.ego_graph(G, ego_node).nodes:
+            colors[ind] = "lightgreen"
+        if node == ego_node:
+            colors[ind] = "lightcoral"
+    return colors
---
+++ textmetrics.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+from collections import Counter
+from distinctiveness.dc import distinctiveness
+
+
+def calculateTextMetrics(G, tokenized_texts):
+    prevalence = calculatePrevalence(texts=tokenized_texts)
+    diversity = calculateDiversity(G=G, reverse=False)
+    connectivity = calculateConnectivity(G=G)
+
+    metrics = pd.DataFrame.from_dict(prevalence, orient="index", columns=["prevalence"])
+    metrics["diversity"] = metrics.index.map(diversity)
+    metrics["connectivity"] = metrics.index.map(connectivity)
+
+    return metrics
+
+
+def calculatePrevalence(texts, reverse=True):
+    # Create a dictionary with frequency counts for each word
+    countPR = Counter()
+    for t in texts:
+        countPR.update(Counter(t))
+
+    # Calculate average score and standard deviation
+    avgPR = np.mean(list(countPR.values()))
+    stdPR = np.std(list(countPR.values()))
+
+    # Calculate standardized Prevalence for each keyword
+    prevalence = {}
+    for t in texts:
+        for kw in t:
+            prevalence[kw] = (countPR[kw] - avgPR) / stdPR
+
+    sortedPrevalence = {k: v for k, v in sorted(prevalence.items(), key=lambda item: item[1], reverse=reverse)}
+
+    return sortedPrevalence
+
+
+def calculateDiversity(G, reverse=True):
+    # Calculate Distinctiveness Centrality
+    DC = distinctiveness(G, normalize=False, alpha=2)
+    DIVERSITY_sequence = DC["D2"]
+
+    # Calculate average score and standard deviation
+    avgDI = np.mean(list(DIVERSITY_sequence.values()))
+    stdDI = np.std(list(DIVERSITY_sequence.values()))
+    # Calculate standardized Diversity for each brand
+    diversity = {}
+    for kw in G.nodes:
+        diversity[kw] = (DIVERSITY_sequence[kw] - avgDI) / stdDI
+
+    sortedDiversity = {k: v for k, v in sorted(diversity.items(), key=lambda item: item[1], reverse=reverse)}
+
+    return sortedDiversity
+
+
+def calculateConnectivity(G, reverse=True):
+    # Define inverse weights, beacaus bc interprets weight as distance
+    for u, v, data in G.edges(data=True):
+        if "weight" in data and data["weight"] != 0:
+            data["inverse"] = 1 / data["weight"]
+        else:
+            data["inverse"] = 1
+
+    CONNECTIVITY_sequence = nx.betweenness_centrality(G, normalized=False, weight="inverse")
+    # Calculate average score and standard deviation
+    avgCO = np.mean(list(CONNECTIVITY_sequence.values()))
+    stdCO = np.std(list(CONNECTIVITY_sequence.values()))
+    # Calculate standardized Prevalence for each brand
+    connectivity = {}
+    for kw in G.nodes:
+        connectivity[kw] = (CONNECTIVITY_sequence[kw] - avgCO) / stdCO
+
+    sortedConnectivity = {k: v for k, v in sorted(connectivity.items(), key=lambda item: item[1], reverse=reverse)}
+
+    return sortedConnectivity
